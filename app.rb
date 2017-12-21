acc32c0ff4351a452f5abd2d63a102cfc8661567
JOHN <Wallacej2@student.lasalle.edu>
2017-12-21 14:01:46 -0500

    fixed p
    fixed indents and margins, fixed pictures to fit the page.

--- app.rb
+++ app.rb
@@ -58,22 +58,3 @@ post "/submit" do
 
   response = sg.client.mail._('send').post(request_body: mail.to_json)
 end
-
-
-#  post "/submit" do
-#   @email = params[:mail]
-#   @message = params[:user_message]
-#
-#   # First, instantiate the Mailgun Client with your API key
-#   mg_client = Mailgun::Client.new 'your api key'
-#
-#   # Define your message parameters
-#   message_params =  { from: @email,
-#                       to:   'admin@associatesofmaple.me',
-#                       subject: 'The Ruby SDK is awesome!',
-#                       text: @user_message
-#                     }
-#
-#   # Send your message through the client
-#   mg_client.send_message 'https://api.mailgun.net/v3/sandbox275a7519749c44b1af047c06143acc83.mailgun.org', message_params
-# end
